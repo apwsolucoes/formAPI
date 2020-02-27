48f1b5439190a356f027e1a3282cb85e410307ae
apwsolucoes <apwsolucoes@gmail.com>
2020-02-26 22:58:19 -0300

    commit 3

--- src/main/java/com/example/formAPI/api/form/FormController.java
+++ src/main/java/com/example/formAPI/api/form/FormController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import com.example.formAPI.domain.FormSAP;
 import com.example.formAPI.domain.dto.RetornoSapDto;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/form")
@@ -27,6 +29,30 @@ public class FormController {
         retorno.setLista(lista1);
 
          return retorno; //ResponseEntity.ok(retorno);
+    }
+
+    @GetMapping("/teste")
+    public String get() {
+
+        String retorno = "OK";
+        String url = "http://localhost:8092/api/v1/form";
+
+        RestTemplate restTemplate = new RestTemplate();
+        FormSAP form = new FormSAP();
+        form.setNome("Paulo M");
+        List<String> lista1 = new ArrayList<String>();
+        lista1.add("TEXTO-111111");
+        lista1.add("TEXTO-222222");
+        lista1.add("TEXTO-333333");
+        form.setListaTexto(lista1);
+
+
+        RetornoSapDto retornoR = restTemplate.postForObject(url, form, RetornoSapDto.class);
+        
+        if(retornoR.getResponse() != "1")
+         return retorno;
+        else
+         return "Erro";
     }
 
 }
--- src/main/java/com/example/formAPI/domain/FormSAP.java
+++ src/main/java/com/example/formAPI/domain/FormSAP.java
@@ -1,11 +1,23 @@
 package com.example.formAPI.domain;
 
+import java.util.List;
+
 public class FormSAP {
 
     private String nome;
 
+    private List<String> listaTexto;
+
     public String getNome() {
         return nome;
+    }
+
+    public List<String> getListaTexto() {
+        return listaTexto;
+    }
+
+    public void setListaTexto(List<String> listaTexto) {
+        this.listaTexto = listaTexto;
     }
 
     public void setNome(String nome) {
